6bba12fe2c360937fc6957f6718961fbb0fef12c
“Matias <matias.n.maidana@gmail.com>
2024-05-06 12:32:33 -0300

    User entity completed

--- src/main/java/com/connect/entities/User.java
+++ src/main/java/com/connect/entities/User.java
@@ -1,13 +1,60 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.connect.entities;
 
-/**
- *
- * @author Asus
- */
+import com.connect.enums.Roles;
+import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorColumn;
+import jakarta.persistence.DiscriminatorType;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import java.util.Date;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "rol",
+        discriminatorType = DiscriminatorType.STRING)
+@DiscriminatorValue("USER")
+@Entity
+
 public class User {
     
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String Id;
+    
+    private String name;
+    private String lastName;
+    private String userName;
+    private String password;
+    
+   @Temporal(TemporalType.DATE)
+    private Date dateOfBirth;
+   
+   @OneToOne
+   private Image image;
+   
+   @OneToOne
+   private Contact contacto;
+   
+   @Enumerated(EnumType.STRING)
+   @Column(name = "rol")
+   private Roles rol;
+   
 }
