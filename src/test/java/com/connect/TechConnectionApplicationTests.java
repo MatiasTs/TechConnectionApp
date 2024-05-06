ebb5dc50ca7c40b5441a1a84915423ebfaf4147d
“Matias <matias.n.maidana@gmail.com>
2024-05-06 09:43:52 -0300

    Proyect Structure

---
+++ src/test/java/com/connect/TechConnectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechConnectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
