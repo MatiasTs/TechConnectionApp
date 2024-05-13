43e02ec567935c61edae64cca28cd997ca78f2e4
“Matias <matias.n.maidana@gmail.com>
2024-05-13 08:45:06 -0300

    Resident Service modify/delete/search/list

--- src/main/java/com/connect/repositories/ResidentRepository.java
+++ src/main/java/com/connect/repositories/ResidentRepository.java
@@ -2,10 +2,15 @@
 package com.connect.repositories;
 
 import com.connect.entities.Resident;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ResidentRepository extends JpaRepository<Resident, String>{
+    
+    @Query("SELECT r FROM Resident r ORDER BY r.userName ASC")
+    List<Resident> residentsOrderByUsername();
     
 }
--- src/main/java/com/connect/services/ResidentService.java
+++ src/main/java/com/connect/services/ResidentService.java
@@ -8,7 +8,10 @@ import com.connect.entities.Resident;
 import com.connect.repositories.ResidentRepository;
 import com.connect.repositories.UserRepository;
 import exception.MiException;
+import jakarta.transaction.Transactional;
 import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -25,7 +28,7 @@ public class ResidentService {
     private UserRepository userRepository;
     
    
-    
+    @Transactional
     public void createResident(String name, String password, String confirmPassword, String lastName,String userName,
             Contact contact, String NIC, Date dateOfBird, Image image, Address address) throws MiException{
         
@@ -47,6 +50,53 @@ public class ResidentService {
         
         residentRepository.save(resident);
         
+    }
+    
+    @Transactional
+    public void modifyResident(String name, String id,String password, String confirmPassword, String lastName,String userName,
+            Contact contact, String NIC, Date dateOfBird, Image image, Address address) throws MiException{
+        
+        userService.validation(name, password, confirmPassword, lastName, userName, contact, NIC, dateOfBird, image);
+        
+        residentValidation(address);
+        
+        Optional<Resident> response = residentRepository.findById(id);
+        
+        if(response.isPresent()){
+            
+            Resident resident = response.get();
+            
+            resident.setName(name);
+            resident.setPassword(password);
+            resident.setLastName(lastName);
+            resident.setUserName(userName);
+            resident.setContacto(contact);
+            resident.setNIC(NIC);
+            resident.setDateOfBirth(dateOfBird);
+            resident.setImage(image);
+            resident.setAddress(address);
+            residentRepository.save(resident);
+        }
+         
+    }
+    
+    
+    @Transactional
+    public void deleteResident(String id){
+        residentRepository.deleteById(id);
+    }
+    
+    public Resident searchById(String id){
+        Optional<Resident> response = residentRepository.findById(id);
+        if(response.isPresent()){
+            Resident resident = response.get();
+            return resident;
+        }
+        return null;
+    }
+    
+    public List<Resident> listResidents(){
+        return residentRepository.residentsOrderByUsername();
     }
     
     
